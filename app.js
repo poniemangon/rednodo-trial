da925addc678f0a5fef6c1420256c9d8b4a8aef0
gon_14 <poniemangon@gmail.com>
2023-11-24 21:58:44 -0300

    app.js

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-
-  const htmlPath = path.join(__dirname, 'rednodo-gappi.html');
-  
-
-  res.sendFile(htmlPath);
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/rednodo-gappi.html');
 });
+
